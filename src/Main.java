dfc55256f0da870fbefd75c495f6af9a0a471788
mintteal <minttu.laesmaa@student.hamk.fi>
2021-12-02 17:16:46 +0200

    Ensimmäinen commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		System.out.println(Laskuri.laskePintaAla(2, 5));
+		System.out.println(Laskuri.laskePintaAla(3));
 
 	}
 
+}
+
+class Laskuri
+{
+	
+	public static double laskePintaAla(double pituus, double leveys)
+	{
+	return pituus*leveys;
+	}
+	
+	public static double laskePintaAla(double sade)
+	{
+		return (22*sade*sade)/7;
+	}
+	
+	
 }
