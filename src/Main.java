dfc52700f5283e296d79973b8254e9d012f70a7d
mintteal <minttu.laesmaa@student.hamk.fi>
2021-12-02 17:19:34 +0200

    Toinen commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main 
 {
@@ -5,9 +6,35 @@ public class Main
 	public static void main(String[] args) 
 	{
 		
-		System.out.println(Laskuri.laskePintaAla(2, 5));
-		System.out.println(Laskuri.laskePintaAla(3));
-
+//		System.out.println(Laskuri.laskePintaAla(2, 5));
+//		System.out.println(Laskuri.laskePintaAla(3));
+		
+		Scanner in = new Scanner (System.in);
+		
+		String answer;
+		double pituus, leveys, sade;
+		
+		System.out.println("Suorakulmio vai ympyrä?");
+		answer = in.nextLine();
+		
+		if(answer.equalsIgnoreCase("suorakulmio"))
+		{
+			System.out.println("Anna pituus");
+			pituus = Double.parseDouble(in.nextLine());
+			
+			System.out.println("Anna leveys");
+			leveys = Double.parseDouble(in.nextLine());
+			
+			System.out.println(Laskuri.laskePintaAla(pituus, leveys));
+		}
+		
+		else
+		{
+			System.out.println("Anna säde");
+			sade = Double.parseDouble(in.nextLine());
+			
+			System.out.println(Laskuri.laskePintaAla(sade));
+		}
 	}
 
 }
